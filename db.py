cf72f61859c6bfc1ec29263f753d4dfa51d6d809
Shiwoo. Min <shiverlog@gmail.com>
2024-09-15 09:24:53 +0900

    cloud db update

--- db.py
+++ db.py
@@ -2,10 +2,13 @@ import mysql.connector
 
 # MySQL 데이터베이스 연결 설정
 db_config = {
-    'host': 'localhost',
-    'user': 'apt',
-    'password': 'apt!1111',
-    'database': 'apt'
+    'user': 'snvtumbceb',
+    'password': 'aptda!1111',  # 실제 비밀번호로 변경
+    'host': 'apt-server.mysql.database.azure.com',
+    'port': 3306,
+    'database': 'apt-database',  # 실제 데이터베이스 이름으로 변경
+    'ssl_ca': '/path/to/ca-cert.pem',  # SSL 인증서의 경로로 변경
+    'ssl_disabled': False
 }
 
 # MySQL 데이터베이스 연결 함수
